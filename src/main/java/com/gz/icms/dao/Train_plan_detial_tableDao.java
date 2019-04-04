9f402a68541300556d425385f7154641ce73a91b
guozhi97 <978540049@qq.com>
2019-04-04 21:34:23 +0800

    update insert

--- src/main/java/com/gz/icms/dao/Train_plan_detial_tableDao.java
+++ src/main/java/com/gz/icms/dao/Train_plan_detial_tableDao.java
@@ -2,7 +2,7 @@
 /*create by gz*/
 package com.gz.icms.dao;
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Update;
